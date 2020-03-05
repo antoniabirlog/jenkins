1b03851a2f66f382adcf7c5f587f4696c105002c
Antonia <antonia.birlog@gmail.com>
2020-03-05 09:53:48 +0200

    changed message

--- src/main/java/com/example/erp/jenkins/demo/HelloWorldController.java
+++ src/main/java/com/example/erp/jenkins/demo/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping("/hello")
 	public String sayHello() {
-		return "Hello World, ERP!!!";
+		return "Hello World, Developer!!!";
 	}
 }
